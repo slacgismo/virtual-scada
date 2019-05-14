364108ab3925457a76c269dd3487d7fe98283082
SerhanK989 <serhan.kiliccote@gmail.com>
2019-05-14 01:07:06 -0700

    Fixed issues with forward fill and non Null Intersection.

--- virtualscada/vs.py
+++ virtualscada/vs.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sys
 sys.path.insert(0, 'C:/Users/Serhan/Documents/SLACwork/VADER-Analytics/mlpowerflow')
 import mlpf
+from sklearn.preprocessing import StandardScaler
+
 
 def removeValues(data, percentage, inplace = False):
     """
@@ -37,27 +39,26 @@ def removeRows(data, rowPercentage, colPercentage = 1, inplace = False):
     numRows, numCols = data.shape
     for j in range(numRows):
         if np.random.uniform() < rowPercentage:
-            for i in numCols:
+            for i in range(numCols):
                 if np.random.uniform() < colPercentage:
                     dataRemoved[i][j] = None
 
     return dataRemoved
 
 
-def nonNullIntersection(dataList,):
+def nonNullIntersection(dataList):
     """
     Takes a list of dataframes and returns the list of dataframes with rows that contain a null value in
     any dataframe removed. This is so that the row can be used for training ML Powerflow
     """
-    dataList = dataList.deepcopy()
 
     masks = []
     for data in dataList:
-        mask = data.isna().any(axis = 1)
-        masks.append(mask)
+        mask = data.isna().any(axis=1)
+        masks.append(~mask)
 
     finalMask = masks[0]
-    for mask in masks:
+    for mask in masks[1:]:
         finalMask = finalMask & mask
 
     for x in range(len(dataList)):
@@ -65,42 +66,62 @@ def nonNullIntersection(dataList,):
 
     return dataList
 
-def fillValuesMLPFForward(p, q, v, a):
+def fillValuesMLPFForward(p, q, v, a, max_iter = 1e3, C_set = [0.01, 0.1, 2.0], eps_set = [1e-7, 1e-5, 1e-3]):
     """
     Takes real and reactive power and voltage and phase angle and trains
     an mlpowerflow model to fill in missing power data
     """
-    num_samples, num_bus = p.shape
 
-    model = mlpf.ForwardMLPF(num_bus, num_samples)
 
-    dataNonNull = nonNullIntersection([p,q,v,a].deepcopy())
+
+    dataNonNull = nonNullIntersection([p,q,v,a])
     pNonNull = dataNonNull[0].values
     qNonNull = dataNonNull[1].values
     vNonNull = dataNonNull[2].values
     aNonNull = dataNonNull[3].values
 
-    model.supply_full_data(pNonNull, qNonNull, vNonNull, aNonNull)
+    num_samples, num_bus = pNonNull.shape
+    model = mlpf.ForwardMLPF(num_bus, num_samples)
 
-    model.train_test_split_data(train_percent = 1)
-    C_set = [0.01, 0.1, 2.0]
-    eps_set = [1e-7, 1e-5, 1e-3]
-    max_iter = 1e3
-    model.fit(C_set, eps_set, max_iter)
+    model.supply_full_data(pNonNull, qNonNull, vNonNull, aNonNull, num_bus, num_samples)
 
-    voltage = pd.concat([v,a])
-    power = pd.concat([p,q])
+    model.train_test_split_data(train_percent = .4)
+
+    model.fit_svr(C_set, eps_set, max_iter)
+    model.test_error_svr()
+    print(model.total_rmse)
+
+    voltage = pd.concat([v,a],axis=1).values
+    power = pd.concat([p,q],axis=1).values
+
+    scaler_x = StandardScaler()
+    scaler_x.fit(model.X_train)
 
     for j in range(num_samples):
         if np.isnan(np.sum(power[j])):
             X = voltage[j]
-            predictions = model.apply_svr(X)
+
+            vj = X[0:30]
+            aj = X[30:]
+            u = vj * np.cos(aj)  # Make sure a is in radians
+            w = vj * np.sin(aj)
+            uw = np.zeros((1, 2 * num_bus))
+            uw[:, np.arange(0, num_bus)] = u
+            uw[:, np.arange(num_bus, 2 * num_bus)] = w
+
+
+            XScaled = scaler_x.transform(uw)
+
+            predictions = model.apply_svr(XScaled)
             for i in range(num_bus):
                 if np.isnan(power[j,i]):
                     power[j,i] = predictions[i]
 
+
     pFilled = pd.DataFrame(power[:, :num_bus])
     qFilled = pd.DataFrame(power[:, num_bus:])
+
+
 
     return pFilled, qFilled
 
